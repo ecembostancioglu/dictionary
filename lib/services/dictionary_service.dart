edd3ef19f713453df6a5354e85c5039624c7cc35
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-10-22 16:30:28 +0300

    Created search word function

--- lib/services/dictionary_service.dart
+++ lib/services/dictionary_service.dart
@@ -1,3 +1,26 @@
+import 'dart:convert';
+
+import 'package:dictionary/models/words.dart';
+import 'package:http/http.dart' as http;
 class DictionaryService{
 
+  Future<List<String>> searchWord(String word)async{
+    Uri url=Uri.parse('https://sozluk.gov.tr/gts?ara=$word');
+    final response=await http.get(url);
+
+    if(response.statusCode == 200) {
+      final result=json.decode(response.body);
+      List<String> means=[];
+
+      if(result[0] != null){
+        Words words=Words.fromJson(result[0]);
+        for(MeansList mean in words.meansList!){
+           means.add(mean.mean!);
+        }
+      }
+      return means;
+    }
+    throw Exception('Bad Request');
+    }
+  }
 }
